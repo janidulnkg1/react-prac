486965ae8185015b4e5774b1c85d32acddea1a52
janidulnkg1 <janidulnkg1@gmail.com>
2023-09-18 11:34:38 +0530

    Add changes
    Adding CSS with blockquote

---
+++ .history/keeptrack/src/App_20230918110818.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <blockquote cite="Benjamin Franklin"></blockquote>
+  );
+}
+
+export default App;
--- .history/keeptrack/src/App_20230918110845.tsx
+++ .history/keeptrack/src/App_20230918110845.tsx
@@ -4,11 +4,9 @@ import './App.css';
 
 function App() {
   return (
-
     <blockquote cite="Benjamin Franklin">
-        Tell me and I forget, teach me and I may remember, involve me and I learn.
+        Tell me and I forget, teach me and I may remember
     </blockquote>
-    
   );
 }
 
